45ec80d2d81cf2eb679935047359a2a3e2a64e2b
Tomasz <70978652+tomaszsiudakrep@users.noreply.github.com>
2024-10-10 14:03:50 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,12 @@
 const CACHE_NAME = 'timer-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/trening-app/',
+    '/trening-app/index.html',
+    '/trening-apptrening-app/style.css',
+    '/trening-app/script.js',
+    '/trening-app/manifest.json',
+    '/trening-app/icons/icon-192x192.png',
+    '/trening-app/icons/icon-512x512.png'
 ];
 
 // Instalowanie Service Workera i dodanie zasobów do cache
